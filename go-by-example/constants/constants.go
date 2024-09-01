a9833c7216cc90cd9b8c3e90de005877e3609748
samin <samintejas@gmail.com>
2024-09-01 23:45:47 +0530

    more go by example content

---
+++ go-by-example/constants/constants.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "global constant"
+func main() {
+ fmt.Println(s)
+ const n = 5000000000
+
+ const d = 3e20/n
+ fmt.Println(d)
+ fmt.Println(int64(d))
+ fmt.Println(math.Sin(n))
+}
+
