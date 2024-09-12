034ddfeb8704ea9f49981b23d77c8a50a15d27d9
samin <samintejas@gmail.com>
2024-09-12 22:53:33 +0530

    if else

--- go-by-example/for/for.go
+++ go-by-example/for/for.go
@@ -4,32 +4,30 @@ import "fmt"
 
 func main() {
 
-    i := 1
-    for i <= 3 {
-        fmt.Println(i)
-        i = i + 1
-    }
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
 
-    for j := 0; j < 3;j++ {
-        fmt.Println(j)
-    }
+	for j := 0; j < 3; j++ {
+		fmt.Println(j)
+	}
 
-    for i := range 3 {
-        fmt.Println("range",i)
-    }
+	for i := range 3 {
+		fmt.Println("range", i)
+	}
 
-    for {
-        fmt.Println("loop")
-        break
-    }
-    for n := range 6 {
-        if n%2 == 0 {
-            continue
-        }
-        fmt.Println(n)
+	for {
+		fmt.Println("loop")
+		break
+	}
+	for n := range 6 {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
 
-    }
-
-
+	}
 
 }
